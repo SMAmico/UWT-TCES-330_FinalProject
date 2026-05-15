12ae705993f943a4a168ff0f4449a05580967304
John R Teal Jr. <jrteal@gmail.com>
2026-05-14 17:00:49 -0700

    Add RegALU module and associated components
    This file contains the register file, ALU, RegALU integration module, and RegALU testbench for Project Phase II.

---
+++ RegALU.sv
@@ -0,0 +1,26 @@
+/*
+Seth Amico, John Teal
+UW TCES 330
+Project Phase II
+15 May 2026
+
+Description:
+This file contains the register file, ALU, RegALU integration module,
+and RegALU testbench for Project Phase II.
+*/
+
+module RegFile (...);
+	...
+endmodule
+
+module ALU (...);
+	...
+endmodule
+
+module RegALU (...);
+	...
+endmodule
+
+module RegALU_tb();
+	...
+endmodule
