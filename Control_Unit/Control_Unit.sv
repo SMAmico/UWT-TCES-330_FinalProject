8a3f20a32cf3025e904a64652c475519aa6eab7f
SMAmico <91100586+SMAmico@users.noreply.github.com>
2026-05-28 12:09:37 -0700

    rough control unit work

--- Control_Unit/Control_Unit.sv
+++ Control_Unit/Control_Unit.sv
@@ -7,81 +7,92 @@
 // thus the code should contain a line 
 // "assign StateOut = State"
 
-module Name( Clk, other input signals, output signals, StateOut);
+module Control_Unit_FSM(
+			// system clock
+			input Clk, 
+			
+			//Reset line
+			input Rst,
+			
+			//PC control lines
+			output PC_clr,
+			output PC_up,
+			//output [7:0]PC_set, //this is a overwrite line for the PC, will be implemented for JMP
+			
+			//IR input lines
+			input [15:0]IR_data,
+			output Id,
+			
+			//RAM control lines
+			output [7:0] D_Addr,
+			output D_wr,
 
-  input Clk;        // system clock
-  input ....        // other inputs including Clr, Reset, etc
+			//Register File source control mux select line
+			output RF_s,
 
-  output logic ...  // system output 
-  output [X:0]StateOut; // if we want to monitor the states of 
-	                  // an FSM, we can add to the portlist 
-		            // an output named StateOut
+			// Register File control lines
+			output [3:0]RF_W_addr,
+			output [3:0]RF_Ra_addr,
+			output [3:0]RF_Rb_addr,
+			output RF_W_en,
+			
+			//ALU control lines
+			output [2:0]Alu_s0,
+			//input Alu_Z //this is an inbuilt ALU flag line that will need to be implemented
+			//input Alu_V //this is an inbuilt ALU overflow flag line
+			//input Alu_N //this is an inbuilt ALU negative flag line
+			
+			//current state output lines
+			output [3:0]StateOut
+			);
 
-  // name your states with localparams
-  // depend on the number of states, assign
-  // approriate values to these localparams
-  // suppose 3 states named A, B, C
-  localparam A  = 2'd0, 
-             B  = 2'd1, 
-             C  = 2'd2;
+  localparam S_INIT = 4'b0000;
+			 S_FETCH = 4'b0001;
+			 S_DEC = 4'b0010;
+			 S_EXE = 4'b0011;
+			 S_NOP = 4'b0100;
+			 S_STR = 4'b0101;
+			 S_LDR = 4'b0110;
+			 S_ADD = 4'b0111;
+			 S_SUB = 4'b1000;
+			 S_HLT = 4'b1001;
+			 //S_XOR = 4'b1010; //extra states from ALU should we choose to implement them
+			 //S_OR  = 4'b1011;
+			 //S_AND = 4'b1100;
+			 
+			 //S_JMP = 4'b1101; //Extra credit. these need to be included in the ALU
+			 //S_JNZ = 4'b1110;
+			 //S_JLT = 4'b1111;
   
-  //notice the state variables should have bit-size
-  //matching the number of states
-  //in this example, 3 states, so 2-bits
-  //if you have N states, then the vector size is
-  //decided by $clog2(N)-1
-  logic [1:0] State, NextState;  // state variables
+  logic [3:0] State, NextState;  // state variables
   
-  //assign the StateOut values the same as State
-  //this step CANNOT skip! Quartus cannot generate 
-  //the right state transition diagram if you combine
-  //the State and StateOut
   assign StateOut = State; 
 
   //CombLogic (use blocking assigns)
   //describe state transition
   //of a Moore machine
   always_comb begin
-    
+	
+	
     case (State)
-      A: begin
-        // specify the output if different from the default
-        if ( any input deciding the state switching)  begin
-          NextState = B; 
+      S_INIT: begin
+        PC_clr = 1;
+        if (1)  begin		//always move to fetch state
+          NextState = S_FETCH; 
         end
       end  
       
-      B: begin
-         // specify the output if different from the default
-        if ( any input deciding the state switching ) begin
-          NextState = C;
-        end
-      end  
-      
-      C: begin
-        //specify the output if different from the default
-	if ( any input deciding the state switching ) begin
-          NextState = A;
-        end
-       end
       
       default: begin
-        // specify the default output 
-        NextState = ;          // safe state
+ 
+        NextState = S_INIT;          // safe state
       end
     endcase //end case - state trnasition description
   end // end the always Comb Logic
     
-  //StateReg (use non-blocking assigns)
-  //this example has an active low reset signal
-  //so name it as 'ResetN'; if an active high
-  //signal, match it with a name Reset
-  //the template given here has the reset signal
-  //synchronized by the Clk signal. Most of the time
-  //if not specified, we make this a default setting
   always_ff @(posedge Clk) begin
-    if (!ResetN)
-      State <= A;
+    if (Rst)
+      State <= S_INIT;
     else
       State <= NextState;   // go to the state we described above
   end // end the always ff logic
