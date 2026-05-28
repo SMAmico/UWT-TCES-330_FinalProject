245ad5d5e3f362c516a4561fe6540d57a387d8c6
John R Teal Jr. <jrteal@gmail.com>
2026-05-28 15:30:03 -0700

    Change input/output types to logic in FSM module

--- Control_Unit/FSM.sv
+++ Control_Unit/FSM.sv
@@ -13,37 +13,37 @@ module Control_Unit_FSM(
 			input Rst,			 //state machine reset line
 			
 			//PC control lines
-			input logic [7:0]PC,
+			input [7:0]PC,
 			output logic PC_clr,		 //PC clear command line
 			output logic PC_up,		 //PC upcounter control line
 			output logic PC_w_en,
 			output logic [7:0]PC_set, //this is a overwrite line for the PC, will be implemented for JMP
 			
 			//IR input lines
-			input logic [15:0]IR_data, //the raw instruction data from ROM
-			output IR_ld,		 //instruction data load command
+			input [15:0]IR_data, //the raw instruction data from ROM
+			output logic IR_ld,		 //instruction data load command
 			
 			//RAM control lines
-			output [7:0] D_Addr, //output address to RAM
-			output D_wr,		 //write enable line to data
+			output logic [7:0] D_Addr, //output address to RAM
+			output logic D_wr,		 //write enable line to data
 
 			//Register File source control mux select line
-			output RF_s,		 //RF source control line
+			output logic RF_s,		 //RF source control line
 
 			// Register File control lines
-			output [3:0]RF_W_addr,	//RF write address 
-			output [3:0]RF_Ra_addr, //RF read address A
-			output [3:0]RF_Rb_addr, //RF read address B
-			output RF_W_en,			//RF write address enable
+			output logic [3:0]RF_W_addr,	//RF write address 
+			output logic [3:0]RF_Ra_addr, //RF read address A
+			output logic [3:0]RF_Rb_addr, //RF read address B
+			output logic RF_W_en,			//RF write address enable
 			
 			//ALU control lines
-			output [2:0]Alu_s0,
+			output logic [2:0]Alu_s0,
 			//input Alu_Z //this is an inbuilt ALU flag line that will need to be implemented
 			//input Alu_V //this is an inbuilt ALU overflow flag line
 			//input Alu_N //this is an inbuilt ALU negative flag line
 			
 			//current state output lines
-			output [3:0]StateOut
+			output logic [3:0]StateOut
 			);
 	
 	//opcode localparams
