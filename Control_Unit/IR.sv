318e58e890a9e7f266f237ce0aee645414c6161b
SMAmico <91100586+SMAmico@users.noreply.github.com>
2026-05-28 13:46:57 -0700

    Update IR.sv

--- Control_Unit/IR.sv
+++ Control_Unit/IR.sv
@@ -1,4 +1,15 @@
 
-module IR(clk, Ir_data, Ir_ld);
+module IR(
+		input clk, 
+		input [7:0]PC_addr,	 //pc address to use for
+		output logic [15:0]Ir_data,//returned data from ROM
+		input Ir_ld			 //trigger line to fetch an instruction from the current PC value
+		
+		);
+		
+	always @(posedge Ir_ld) begin
+		
+	end
+		
 endmodule
 
