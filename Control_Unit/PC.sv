0b411b37e2a878d2cc732517f4b41a50c04b6711
John R Teal Jr. <jrteal@gmail.com>
2026-05-30 16:26:37 -0700

    Refactor PC module comments and variable names
    Updated comments and variable naming in the PC module for clarity.

--- Control_Unit/PC.sv
+++ Control_Unit/PC.sv
@@ -1,19 +1,30 @@
-//tces 330
-//Project File : PC register
+/*
+Seth Amico, John Teal
+UW TCES 330
+Programmable Processor
+Project File: PC.sv
+10 June 2026
+*/
 
 module PC(
-		input clk,
-		input PC_clr,
-		input PC_up,
-		output logic [7:0] PC_out
-		);
-	
-	always_ff @(posedge clk) begin
+    input Clk,
+    input PC_clr,
+    input PC_up,
+	output logic [7:0] PC_out
+);
+
+    /*
+    The PC module stores the current instruction address. PC_clr and PC_up are control signals from 
+	the FSM. They are not clocks. The PC only changes on the rising edge of clk. PC_clr = 1 clears 
+	the PC to 0. PC_up  = 1 increments the PC by 1. If both PC_clr and PC_up are 0, the PC holds its 
+	current value. PC_clr has priority over PC_up because the reset/init behavior should force the 
+	processor to start from instruction address 0.
+    */
+
+    always_ff @(posedge clk) begin
         if (PC_clr)
-            PC_Out <= 8'b0;
+            PC_out <= 8'b0;
         else if (PC_up)
-            PC_Out <= PC_Out + 8'b1;
+            PC_out <= PC_out + 8'b1;
     end
-
 endmodule
-
