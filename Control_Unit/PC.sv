8dd1f24ddd0c1bb4f1ecfa251093a35710a55610
John R Teal Jr. <jrteal@gmail.com>
2026-05-30 17:13:21 -0700

    Enhance PC module with new inputs and improved comments
    Updated the PC module to include PC_w_en and PC_set inputs. Modified comments for clarity and corrected signal names.

--- Control_Unit/PC.sv
+++ Control_Unit/PC.sv
@@ -10,21 +10,26 @@ module PC(
     input Clk,
     input PC_clr,
     input PC_up,
-	output logic [7:0] PC_out
+    input PC_w_en,
+    input [7:0] PC_set,
+    output logic [7:0] PC_out
 );
 
     /*
-    The PC module stores the current instruction address. PC_clr and PC_up are control signals from 
-	the FSM. They are not clocks. The PC only changes on the rising edge of clk. PC_clr = 1 clears 
-	the PC to 0. PC_up  = 1 increments the PC by 1. If both PC_clr and PC_up are 0, the PC holds its 
-	current value. PC_clr has priority over PC_up because the reset/init behavior should force the 
-	processor to start from instruction address 0.
+    The PC module stores the current instruction address. PC_clr, PC_up, and PC_w_en are control 
+	signals from the FSM. They are not clocks. The PC only changes on the rising edge of Clk.
+    PC_clr  = 1 clears the PC to 0. PC_w_en = 1 loads PC_set into the PC for jump instructions.
+    PC_up   = 1 increments the PC by 1. Priority order: 1. Clear the PC. 2. Load a jump address.
+    3. Increment the PC. 4. Otherwise hold the current PC value.
     */
 
-    always_ff @(posedge clk) begin
+    always_ff @(posedge Clk) begin
         if (PC_clr)
             PC_out <= 8'b0;
+        else if (PC_w_en)
+            PC_out <= PC_set;
         else if (PC_up)
             PC_out <= PC_out + 8'b1;
     end
+
 endmodule
