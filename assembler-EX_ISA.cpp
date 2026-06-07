dda049f12680399254f0f0ec0ff9d1e22d3b141a
SMAmico <91100586+SMAmico@users.noreply.github.com>
2026-06-07 10:59:06 -0700

    comment and clarify the slopcode assembler

--- assembler-EX_ISA.cpp
+++ assembler-EX_ISA.cpp
@@ -65,40 +65,58 @@
 #include <vector>
 using namespace std;
 
+//function cleans each line so it's just the instruction
 static inline string trim(const string &s) {
+
     size_t a = s.find_first_not_of(" \t\r\n");
     if (a==string::npos) return "";
     size_t b = s.find_last_not_of(" \t\r\n");
     return s.substr(a, b-a+1);
 }
 
+//takes in string, returns a vector of string chunks (tokens)
 static inline vector<string> split_tokens(const string &line) {
+    //create vector
     vector<string> toks;
     string cur;
+    //for each 
     for (size_t i=0;i<line.size();) {
-        if (isspace((unsigned char)line[i]) || line[i]==',') { i++; continue; }
+        if (isspace((unsigned char)line[i]) || line[i]==',') {
+            i++;
+            continue;
+            }
         if (line[i]=='/' && i+1<line.size() && line[i+1]=='/') break;
+
+        //break at line end or comment
         if (line[i]==';' || line[i]=='#') break;
         // token: read until whitespace or comma
         size_t j=i;
+        //splits each line into tokens fenceposted by commas
         while (j<line.size() && !isspace((unsigned char)line[j]) && line[j]!=',') j++;
+        //adds the token to the vector based on its bounds
         toks.push_back(line.substr(i, j-i));
         i=j;
     }
+    //return the tokenized string
     return toks;
 
 }
 
+//converts RN register terminology to direct register address
+//input: token equivalent of register
+//output: register value
 int parse_reg(const string &tok) {
+    //takes register
     string s = tok;
     for (auto &c: s) c = toupper((unsigned char)c);
+    //reads until R then converts following integer to hex
     if (s.size()>0 && s[0]=='R') {
         string num = s.substr(1);
         int v = stoi(num);
         if (v<0||v>15) throw runtime_error("register out of range: "+tok);
         return v;
     }
-    // allow raw numbers 0..15
+    // as an alternate input, allow raw numbers 0-15 to convert properly too.
     {
         int v = stoi(s);
         if (v<0||v>15) throw runtime_error("register out of range: "+tok);
@@ -106,11 +124,14 @@ int parse_reg(const string &tok) {
     }
 }
 
+//converts number strings to integers
 int parse_number(const string &tok) {
     string s = tok;
+    //converts hex inputs (ie 0x4FD) to integer properly
     if (s.size()>1 && s[0]=='0' && (s[1]=='x' || s[1]=='X')) {
         return stoi(s,nullptr,16);
     }
+    //catch negative values too
     if (s.size()>1 && s[0]=='-') {
         return stoi(s,nullptr,0);
     }
@@ -118,15 +139,20 @@ int parse_number(const string &tok) {
     return stoi(s,nullptr,0);
 }
 
+//main loop
 int main(int argc, char** argv) {
+    //print input
     if (argc<3) {
         cerr<<"Usage: "<<argv[0]<<" <input.asm> <output.txt>\n";
         return 1;
     }
+    //the paths for our input and output files
     string inpath = argv[1];
     string outpath = argv[2];
 
+    //the raw lines as a vector
     vector<string> lines;
+
     {
         ifstream ifs(inpath);
         if (!ifs) { cerr<<"Cannot open "<<inpath<<"\n"; return 1; }
@@ -135,36 +161,66 @@ int main(int argc, char** argv) {
     }
 
     // First pass: collect labels and normalize lines
+    // traverse the full length of the file twice to properly
+    // capture jumps to labels, etc
+
+    // dump all labels into a map the we create here
     unordered_map<string,int> labels;
-    vector<string> norm_lines; // lines without labels/comments
+
+    
+    vector<string> norm_lines; 
     int addr = 0;
+
     for (size_t i=0;i<lines.size();++i) {
+
+        // fetch X line from the vector
         string l = lines[i];
-        // remove comments
+        // mark out the locations of the important characters
+        // in a line, ( #, //, ; ). 
         size_t cpos = l.find("//");
         size_t cpos2 = l.find(';');
+
+        // if a semicolon is present and before a comment, set the semicolon as the EOL
         if (cpos2!=string::npos && (cpos==string::npos || cpos2<cpos)) cpos = cpos2;
+
         size_t cpos3 = l.find('#');
+
+        // if a hashtag is present and before a comment, set the hashtag as the EOL
         if (cpos3!=string::npos && (cpos==string::npos || cpos3<cpos)) cpos = cpos3;
+
+        //if the comment indicator exists, trim line length to remove it
         if (cpos!=string::npos) l = l.substr(0,cpos);
+
+        //pull off spaces
         l = trim(l);
+        // ignore empty lines
         if (l.empty()) continue;
         // label?
         if (l.back()==':') {
+            // trim label to just title
             string lab = trim(l.substr(0,l.size()-1));
+            // ignore it if empty
             if (lab.empty()) continue;
+            // if it exists already, throw an error
             if (labels.find(lab)!=labels.end()) {
                 cerr<<"Duplicate label "<<lab<<"\n"; return 1; }
+
+            // store the address at the label
             labels[lab] = addr;
             continue;
         }
         // inline label at start: lab: instr
         size_t colon = l.find(':');
+        // if the colon exists
         if (colon!=string::npos) {
+            // trim the label again
             string lab = trim(l.substr(0,colon));
+            // grab the rest of the string
             string rest = trim(l.substr(colon+1));
+            // store the labels again
             labels[lab] = addr;
             l = rest;
+            // repeat until all labels are processed
             if (l.empty()) continue;
         }
         norm_lines.push_back(l);
@@ -182,8 +238,12 @@ int main(int argc, char** argv) {
         for (auto &c: op) c = toupper((unsigned char)c);
         uint16_t instr = 0;
         try {
+
+            //NOP: no operation
             if (op=="NOP") {
                 instr = 0x0000;
+
+            //STR: store register
             } else if (op=="STR") {
                 if (toks.size()<3) throw runtime_error("STR expects R,ADDR");
                 int r = parse_reg(toks[1]);
@@ -193,6 +253,8 @@ int main(int argc, char** argv) {
                 else a = parse_number(toks[2]);
                 if (a<0||a>255) throw runtime_error("address out of range");
                 instr = (0x1<<12) | (r<<8) | (a & 0xFF);
+
+            //LDR: load register
             } else if (op=="LDR" || op=="LOAD") {
                 if (toks.size()<3) throw runtime_error("LDR expects ADDR,R");
                 int a;
@@ -201,28 +263,42 @@ int main(int argc, char** argv) {
                 int r = parse_reg(toks[2]);
                 if (a<0||a>255) throw runtime_error("address out of range");
                 instr = (0x2<<12) | ((a & 0xFF)<<4) | (r & 0xF);
+
+            //ADD: add two registers into a third
             } else if (op=="ADD") {
                 if (toks.size()<4) throw runtime_error("ADD expects RA,RB,RC");
                 int ra=parse_reg(toks[1]); int rb=parse_reg(toks[2]); int rc=parse_reg(toks[3]);
                 instr = (0x3<<12) | (ra<<8) | (rb<<4) | rc;
+
+            //SUB: subtract two registers into a third
             } else if (op=="SUB") {
                 if (toks.size()<4) throw runtime_error("SUB expects RA,RB,RC");
                 int ra=parse_reg(toks[1]); int rb=parse_reg(toks[2]); int rc=parse_reg(toks[3]);
                 instr = (0x4<<12) | (ra<<8) | (rb<<4) | rc;
+
+            //HLT: stop the processor
             } else if (op=="HLT" || op=="HALT") {
                 instr = (0x5<<12);
+
+            //XOR: perform exclusive OR operation on two registers into a third
             } else if (op=="XOR") {
                 if (toks.size()<4) throw runtime_error("XOR expects RA,RB,RC");
                 int ra=parse_reg(toks[1]); int rb=parse_reg(toks[2]); int rc=parse_reg(toks[3]);
                 instr = (0x6<<12) | (ra<<8) | (rb<<4) | rc;
+
+            //OR: perform OR operation on two registers into a third
             } else if (op=="OR") {
                 if (toks.size()<4) throw runtime_error("OR expects RA,RB,RC");
                 int ra=parse_reg(toks[1]); int rb=parse_reg(toks[2]); int rc=parse_reg(toks[3]);
                 instr = (0x7<<12) | (ra<<8) | (rb<<4) | rc;
+
+            //AND: perform AND operation on two registers into a third
             } else if (op=="AND") {
                 if (toks.size()<4) throw runtime_error("AND expects RA,RB,RC");
                 int ra=parse_reg(toks[1]); int rb=parse_reg(toks[2]); int rc=parse_reg(toks[3]);
                 instr = (0x8<<12) | (ra<<8) | (rb<<4) | rc;
+
+            //JMP: jump program counter to a direct address (within 256 words)
             } else if (op=="JMP") {
                 if (toks.size()<2) throw runtime_error("JMP expects ADDR");
                 int a;
@@ -230,6 +306,8 @@ int main(int argc, char** argv) {
                 else a = parse_number(toks[1]);
                 if (a<0||a>255) throw runtime_error("JMP address out of range");
                 instr = (0x9<<12) | (0<<8) | (a & 0xFF);
+
+            //JNZ: conditional branch on register not equal to zero to a direct address (within 256 words)
             } else if (op=="JNZ") {
                 if (toks.size()<3) throw runtime_error("JNZ expects ADDR,REG");
                 int a;
@@ -238,6 +316,8 @@ int main(int argc, char** argv) {
                 int r = parse_reg(toks[2]);
                 if (a<0||a>255) throw runtime_error("JNZ address out of range");
                 instr = (0xA<<12) | ((a & 0xFF)<<4) | (r & 0xF);
+
+            //JNZ: conditional branch on register not equal to zero to a direct address (within 256 words)
             } else if (op=="JLT") {
                 if (toks.size()<4) throw runtime_error("JLT expects RA,RB,OFFSET_OR_LABEL");
                 int ra = parse_reg(toks[1]);
@@ -253,10 +333,15 @@ int main(int argc, char** argv) {
                 if (offset < -8 || offset > 7) throw runtime_error("JLT offset out of range (-8..7)");
                 uint16_t ob = (uint16_t)(offset & 0xF);
                 instr = (0xB<<12) | (ra<<8) | (rb<<4) | ob;
+
+            // SHL: shifts register left by one bit into a second register
             } else if (op=="SHL") {
-                if (toks.size()<4) throw runtime_error("SHL expects RA,RB,RC");
-                int ra=parse_reg(toks[1]); int rb=parse_reg(toks[2]); int rc=parse_reg(toks[3]);
-                instr = (0xC<<12) | (ra<<8) | (rb<<4) | rc;
+                if (toks.size()<3) throw runtime_error("SHL expects RA,RB");
+                int ra=parse_reg(toks[1]);
+                int rb=parse_reg(toks[2]);
+                instr = (0xC<<12) | (ra<<8) | (rb<<4);
+
+            //MULT: the heaviest ALU operation. multiplies two registers and puts result into a third register.
             } else if (op=="MULT") {
                 if (toks.size()<4) throw runtime_error("MULT expects RA,RB,RC");
                 int ra=parse_reg(toks[1]); int rb=parse_reg(toks[2]); int rc=parse_reg(toks[3]);
@@ -264,6 +349,7 @@ int main(int argc, char** argv) {
             } else {
                 throw runtime_error(string("Unknown opcode: ")+op);
             }
+
         } catch (exception &e) {
             cerr<<"Error at instruction "<<addr<<": "<<e.what()<<" -> '"<<rawline<<"'\n";
             return 1;
